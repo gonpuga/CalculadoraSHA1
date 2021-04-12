0af0e21959788665045aa0edfc8088c0e9eab3e4
gonpuga <gonpuga@gmail.com>
2021-04-12 10:28:33 +0200

    SHA1 Hash Generator

--- app/src/main/java/com/example/calculadorasha1/MainActivity.java
+++ app/src/main/java/com/example/calculadorasha1/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.calculadorasha1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -13,57 +11,72 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-        implements ResultCallback<String> {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements ResultCallback<String> {
+
     Sha1HashService mService;
     boolean mBound = false;
 
-    @Override public void onCreate(Bundle savedInstanceState) {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button queryButton = findViewById(R.id.hashIt);
         queryButton.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
                 EditText et = findViewById(R.id.text);
                 if (mService != null) {
-                    mService.getSha1Digest(et.getText().toString(),
-                            MainActivity.this);
+                    mService.getSha1Digest(et.getText().toString(), MainActivity.this);
                 }
             }
         });
+
     }
 
-    @Override public void onResult(String data) {
+    @Override
+    public void onResult(String data) {
         TextView et = findViewById(R.id.hashResult);
         et.setText(data);
     }
 
-    @Override protected void onStart() {
+    @Override
+    protected void onStart() {
         super.onStart();
+        // Bind to LocalService
         Intent intent = new Intent(this, Sha1HashService.class);
         bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
     }
 
-    @Override protected void onStop() {
+    @Override
+    protected void onStop() {
         super.onStop();
+        // Unbind from the service
         if (mBound) {
             unbindService(mConnection);
             mBound = false;
         }
     }
 
+    /** Defines callbacks for service binding, passed to bindService() */
     private ServiceConnection mConnection = new ServiceConnection() {
-        @Override public void onServiceConnected(ComponentName name,
-                                                 IBinder service) {
-            Sha1HashService.LocalBinder binder=
-                    (Sha1HashService.LocalBinder) service;
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            Sha1HashService.LocalBinder binder = (Sha1HashService.LocalBinder) service;
             mService = binder.getService();
             mBound = true;
         }
 
-        @Override public void onServiceDisconnected(ComponentName arg0) {
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
             mBound = false;
             mService= null;
         }
     };
+
+
 }
+
